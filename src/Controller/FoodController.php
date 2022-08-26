60f1d22a84700840bcbcb39d79ced6b814ba0cec
Vaibhav_Garsondiya <83468782+vpgarsondiya@users.noreply.github.com>
2022-08-26 19:26:54 +0530

    Init

---
+++ src/Controller/FoodController.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Food;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FoodController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index(): Response
+    {
+        // return $this->json([
+        //     'message' => 'Welcome to your new controller!',
+        //     'path' => 'src/Controller/FoodController.php',
+        // ]);
+
+        return $this->render('index.html.twig');
+    }
+
+
+    /**
+     * @Route("/all", name="all")
+     */
+    public function all( Request $request, PaginatorInterface $paginator): Response
+    {
+        $tasks = $this->getDoctrine()->getRepository(Food::class)->findBy([],['id'=>'DESC']);
+
+        $paginatedTasks = $paginator->paginate($tasks, $request->query->getInt('page', 1), 5);
+
+        return $this->render('all.html.twig', ['tasks' => $paginatedTasks]);
+    }
+
+
+     /**
+     * @Route("/create", name="create-task", methods={"POST"})
+     */
+    public function create(Request $request) {
+
+        $name = $request->request->get('task');
+
+        $price = $request->request->get('price');
+
+        $stock = $request->request->get('stock');
+
+        $description = $request->request->get('description');
+
+
+
+        $objectManager = $this->getDoctrine()->getManager();
+
+        $lastTask = $objectManager->getRepository(Food::class)->findOneBy([], ['id' => 'desc']);
+
+        $lastId = $lastTask->getId();
+
+        $newId = $lastId + 1;
+
+        $task = new Food;
+
+        $task->setId($newId);
+
+        $task->setName($name);
+
+        $task->setPrice($price);
+
+        $task->setStock($stock);
+
+        $task->setDescription($description);
+
+        $objectManager->persist($task);
+
+        $objectManager->flush();
+
+        $this->addFlash('success', 'You have created a new task!');
+
+        return $this->redirectToRoute('all');
+
+    }
+
+     /**
+     * @Route("/updateStatus/{id}", name="update-status")
+     */
+    public function updateTaskStatus($id) {
+
+        $objectManager = $this->getDoctrine()->getManager();
+
+        $task = $objectManager->getRepository(Food::class)->find($id);
+
+        $task->setStatus(!$task->isStatus());
+
+        $objectManager->flush();
+
+        $this->addFlash('info', 'You have updated a task!');
+
+        return $this->redirectToRoute('all');
+
+    }
+
+    /**
+     * @Route("/deleteTask/{id}", name="delete-task")
+     */
+    public function delete(Food $id) {
+
+        $objectManager = $this->getDoctrine()->getManager();
+
+        $objectManager->remove($id);
+
+        $objectManager->flush();
+
+        $this->addFlash('danger', 'You have deleted a task!');
+
+        return $this->redirectToRoute('all');
+
+    }
+
+
+}
---
+++ src/Entity/Food.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FoodRepository;
+use Symfony\Component\Validator\Constraints as Assert;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=FoodRepository::class)
+ */
+class Food
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @Assert\NotBlank
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank
+     */
+    private $price;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank
+     */
+    private $stock;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="boolean")
+     * 
+     */
+    private $status;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPrice(): ?string
+    {
+        return $this->price;
+    }
+
+    public function setPrice(string $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getStock(): ?string
+    {
+        return $this->stock;
+    }
+
+    public function setStock(string $stock): self
+    {
+        $this->stock = $stock;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function isStatus(): ?bool
+    {
+        return $this->status;
+    }
+
+    public function setStatus(bool $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+}
